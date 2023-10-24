3e0c0cd6bc0ad6bd5a2b397fe732abe6ab265d7e
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-24 14:47:08 -0400

    Initial commit - Completion of Section A

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+1. How do we assign a value to a variable?
+With the assignment operator (=)
+2. How do we change the value of a variable?
+Using an assignment operator (=) and a new
+value for the variable
+3. How do we assign an existing variable to a new variable?
+Use the new variable name, then the assignment operator (=),
+and then the existing variable name
+4. Remind me, what are declare, assign, and define?
+Declare: creating a variable
+Assign: giving the variable a value
+Define: indicating the variable's type
+5. What is pseudocoding and why should you do it?
+Creating a description of a situation before writing
+the actual code. It's best for planning how best to
+lay out the code.
+6. What percentage of time should be spent thinking 
+about how you're going to solve a problem vs 
+actually typing in code to solve it?
+80/20 - 80% thinking 20% typing
+*/
