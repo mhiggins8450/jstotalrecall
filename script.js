ee634a07466f313d7d80d28ad8a869987c96b886
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-24 22:15:31 -0400

    Completion of Section C

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// I. Variables & Datatypes
 // Section A
 /*
 1. How do we assign a value to a variable?
@@ -45,3 +46,28 @@ actually typing in code to solve it?
         // let yourName = 'Michael'
         // let hello = 'Hello, my name is ' + yourName + '.';
         // console.log (hello)
+
+// Section C
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+//   console.log(a < b);
+//   console.log(c > d);
+//   console.log('Name' === 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+    //  console.log(true || false);
+    //  console.log(false && false && false && false && false || true);
+//   console.log(false === false)
+//   console.log(e == 'Kevin');
+    //  console.log((a + b) === c); 
+  // note: a < b < c is NOT CORRECT 
+  //(and is not a valid JS expression, 
+  //think about using other math operations)
+    //  console.log((a * a) === d); 
+  // note: the answer is a simple arithmetic 
+  //equation, not something "weird"
+//   console.log(48 == '48');
